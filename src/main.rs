e76c4de72e553dc94929ef45844a0d4217498593
rizkydenianto <rizkydenianto0@gmail.com>
2025-01-08 14:48:51 +0700

    Uji coba peti OpenCV

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use opencv::{core::{Mat, MatTraitConst}, highgui, videoio::{VideoCapture, VideoCaptureTrait, CAP_ANY}, Result};
+
+fn main() -> Result<()> {
+    let mut video = VideoCapture::from_file("video.mkv", CAP_ANY);
+    if !video.is_ok() {
+        panic!("Unable to open the video file!");
+    }
+
+    loop {
+        let mut frame = Mat::default();
+        video.as_mut().unwrap().read(&mut frame).unwrap();
+        if frame.empty() {break}
+
+        highgui::imshow("frame", &frame).unwrap();
+    }
+
+	Ok(())
 }
