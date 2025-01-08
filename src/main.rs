9079ab5263faa9c48d2e80848bb16df6f7d3c5ad
Made <wisnukusuma99@gmail.com>
2025-01-08 17:24:37 +0700

    perbaharui metode deteksi kendaraan

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,67 @@
 use opencv::{
-    core::{Mat, MatTraitConst},
-    highgui,
+    core::{self, Rect},
+    highgui, imgproc,
+    objdetect::CascadeClassifier,
     prelude::*,
-    videoio::{VideoCapture, VideoCaptureTrait},
-    Result,
+    videoio,
 };
 
-fn main() -> Result<()> {
-    // Muat berkas video
-    let mut video = VideoCapture::from_file(
-        "/home/made/projects/spl3mpc/data_latih/video2.mkv",
-        opencv::videoio::CAP_FFMPEG,
-    )?;
-    if !video.is_opened()? {
-        panic!("Video tidak bisa dimuat!");
+fn main() -> opencv::Result<()> {
+    let cascade_src = "/home/made/projects/spl3mpc/src/cars.xml";
+    let video_src = "/home/made/projects/spl3mpc/data_latih/video.mp4";
+
+    // Open the video file
+    let mut cap = videoio::VideoCapture::from_file(video_src, videoio::CAP_ANY)?;
+    if !cap.is_opened()? {
+        panic!("Error opening video file!");
     }
+
+    // Load the cascade classifier
+    let mut car_cascade = CascadeClassifier::new(cascade_src)?;
 
     loop {
-        // Ambil tiap frame untuk ditampilkan
         let mut frame = Mat::default();
-        video.read(&mut frame)?;
-        if frame.empty() {
+        if !cap.read(&mut frame)? || frame.empty() {
             break;
         }
-        highgui::imshow("frame", &frame)?;
 
-        // Tambah delay agar gambar dapat ditampilkan
-        if highgui::wait_key(20)? >= 0 {
+        // Convert the frame to grayscale
+        let mut gray = Mat::default();
+        imgproc::cvt_color(&frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+
+        // Detect cars in the frame
+        let mut cars = core::Vector::<Rect>::new();
+        car_cascade.detect_multi_scale(
+            &gray,
+            &mut cars,
+            1.1,
+            1,
+            0,
+            core::Size::new(30, 30),
+            core::Size::default(),
+        )?;
+
+        // Draw rectangles around detected cars
+        for car in cars {
+            imgproc::rectangle(
+                &mut frame,
+                car,
+                core::Scalar::new(0.0, 0.0, 255.0, 0.0),
+                2,
+                imgproc::LINE_8,
+                0,
+            )?;
+        }
+
+        // Display the frame
+        highgui::imshow("video", &frame)?;
+
+        // Exit if the Esc key is pressed
+        if highgui::wait_key(33)? == 27 {
             break;
         }
     }
+
+    highgui::destroy_all_windows()?;
     Ok(())
 }
