be87f8505231edd65238a4ce2d26fee996a7c78c
rizkydenianto <rizkydenianto0@gmail.com>
2025-01-09 10:33:16 +0700

    Create a structure for a method

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,14 @@
-use opencv::{
-    core::{Mat, MatTraitConst},
-    highgui,
-    prelude::*,
-    videoio::{VideoCapture, VideoCaptureTrait},
-    Result,
-};
+use method::motion_detection::main::MotionDetection;
+use opencv::{videoio::VideoCapture, Result};
+
+mod method;
 
 fn main() -> Result<()> {
-    // Muat berkas video
-    let mut video = VideoCapture::from_file(
+    let video = VideoCapture::from_file(
         "/home/made/projects/spl3mpc/data_latih/video2.mkv",
         opencv::videoio::CAP_FFMPEG,
     )?;
-    if !video.is_opened()? {
-        panic!("Video tidak bisa dimuat!");
-    }
+    MotionDetection::create(video);
 
-    loop {
-        // Ambil tiap frame untuk ditampilkan
-        let mut frame = Mat::default();
-        video.read(&mut frame)?;
-        if frame.empty() {
-            break;
-        }
-        highgui::imshow("frame", &frame)?;
-
-        // Tambah delay agar gambar dapat ditampilkan
-        if highgui::wait_key(20)? >= 0 {
-            break;
-        }
-    }
     Ok(())
 }
