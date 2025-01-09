b3aca239689a54f7e0232f600ddcb04ffee6354f
rizkydenianto <rizkydenianto0@gmail.com>
2025-01-09 12:02:57 +0700

    Move the code to a Struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-use method::motion_detection::main::MotionDetection;
-use opencv::{videoio::VideoCapture, Result};
+use method::motion_detection::motion_detection_process;
+use opencv::{
+    prelude::*,
+    videoio::VideoCapture,
+    Result,
+};
 
 mod method;
 
 fn main() -> Result<()> {
+    // Muat berkas video
     let video = VideoCapture::from_file(
-        "/home/made/projects/spl3mpc/data_latih/video2.mkv",
+        "/home/wsl/Proyek/spl3mpc/data_latih/2025-01-08 13-22-42.mkv",
         opencv::videoio::CAP_FFMPEG,
     )?;
-    MotionDetection::create(video);
+    if !video.is_opened()? {
+        panic!("Video tidak bisa dimuat!");
+    }
 
+    motion_detection_process(video)?;
     Ok(())
 }
--- src/method/motion_detection/main.rs
+++ src/method/motion_detection/main.rs
@@ -1,11 +1,113 @@
-use opencv::videoio::VideoCapture;
+use opencv::{
+    core::{absdiff, Mat, MatExprTraitConst, Point, Scalar, Size, Vector, BORDER_DEFAULT, CV_8U},
+    imgproc::{self, COLOR_BGR2GRAY},
+    videoio::{VideoCapture, VideoCaptureTrait},
+    Result,
+};
 
 pub struct MotionDetection {
-    source: VideoCapture,
+    pub source: VideoCapture,
+    pub frame: Mat,
+    pub gray_frame: Mat,
+    pub prev_frame: Mat,
+    pub diff_frame: Mat,
+    pub thresh_frame: Mat,
 }
 
 impl MotionDetection {
     pub fn create(source: VideoCapture) -> Self {
-        Self { source }
+        Self {
+            source,
+            frame: Mat::default(),
+            gray_frame: Mat::default(),
+            prev_frame: Mat::default(),
+            diff_frame: Mat::default(),
+            thresh_frame: Mat::default(),
+        }
+    }
+
+    pub fn take_frame(&mut self) -> Result<()> {
+        self.source.read(&mut self.frame)?;
+        Ok(())
+    }
+
+    pub fn create_grayscale_frame(&mut self) -> Result<()> {
+        imgproc::cvt_color(&self.frame, &mut self.gray_frame, COLOR_BGR2GRAY, 0)?;
+        Ok(())
+    }
+
+    pub fn apply_gaussian_blur(&mut self, clone: Mat) -> Result<()> {
+        imgproc::gaussian_blur(
+            &clone,
+            &mut self.gray_frame,
+            Size::new(5, 5),
+            0.0,
+            0.0,
+            BORDER_DEFAULT,
+        )?;
+        Ok(())
+    }
+
+    pub fn calculate_diff_frame(&mut self) -> Result<()> {
+        absdiff(&self.prev_frame, &self.gray_frame, &mut self.diff_frame)?;
+        Ok(())
+    }
+
+    pub fn create_thresh_frame(&mut self) -> Result<()> {
+        imgproc::threshold(
+            &self.diff_frame,
+            &mut self.thresh_frame,
+            25.0,
+            255.0,
+            imgproc::THRESH_BINARY,
+        )?;
+        Ok(())
+    }
+
+    pub fn apply_dilation(&mut self, clone: Mat) -> Result<()> {
+        let kernel = Mat::ones(3, 3, CV_8U)?.to_mat()?;
+        imgproc::erode(
+            &clone,
+            &mut self.thresh_frame,
+            &kernel,
+            Point::new(-1, -1),
+            1,
+            0,
+            Scalar::all(0.0),
+        )?;
+        imgproc::dilate(
+            &clone,
+            &mut self.thresh_frame,
+            &kernel,
+            Point::new(-1, -1),
+            2,
+            0,
+            Scalar::all(0.0),
+        )?;
+        Ok(())
+    }
+
+    pub fn draw_rectangle(&mut self) -> Result<()> {
+        let mut contours: Vector<Mat> = Vector::new();
+        imgproc::find_contours(
+            &self.thresh_frame,
+            &mut contours,
+            imgproc::RETR_EXTERNAL,
+            imgproc::CHAIN_APPROX_SIMPLE,
+            Point::new(0, 0),
+        )?;
+
+        for contour in contours.iter() {
+            let rect = imgproc::bounding_rect(&contour)?;
+            imgproc::rectangle(
+                &mut self.frame,
+                rect,
+                Scalar::new(0.0, 255.0, 0.0, 0.0),
+                2,
+                imgproc::LINE_8,
+                0,
+            )?;
+        }
+        Ok(())
     }
 }
